8d56b6ab5bfbdda25a8338faea53eb34266df1d7
Lidm3025 <78723444+Lidm3025@users.noreply.github.com>
2022-04-10 18:02:09 +0800

    Update mnist_train.py

--- mnist_train.py
+++ mnist_train.py
@@ -250,7 +250,7 @@ def train():
 
                     if test_acc > best_acc:
                         best_acc, best_lr, best_hidden, best_alpha, best_epoch = test_acc, lr, hidden, alpha, epoch
-                        net.save_model('2level.npy')
+                        net.save_model('2layer.npy')
     print('best_lr: ', best_lr, 'best_hidden: ', best_hidden, 'best_alpha: ', best_alpha, 'best_epoch: ', best_epoch,' best_acc: ', best_acc)
 
 
